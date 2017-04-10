2dc8fdea86a770db06f5a6c3b5dc43375e817ff5
like <416960428@qq.com>
2017-04-10 16:38:24 +0800

    'sth'

--- 17codes/build/dev-server.js
+++ 17codes/build/dev-server.js
@@ -1,4 +1,8 @@
 var express = require('express')
 var webpack = require('webpack')
 // 创建一个express实例
-var app = express()
+var app = express();
+debugger;
+console.log(1);
+//__dirname:"d:\MyProject\17codes\17codes\build"
+//__filename:"d:\MyProject\17codes\17codes\build\dev-server.js"
--- 17codes/build/dev.webpack.config.js
+++ 17codes/build/dev.webpack.config.js
@@ -4,7 +4,7 @@ var HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     entry: {
-      
+        app: ['./static/src/main']
     },
     output: {
        
