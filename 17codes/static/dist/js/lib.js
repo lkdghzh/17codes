ab1a20787e0d1ab64306df00f6fd53d2f069fb68
like <416960428@qq.com>
2017-04-13 15:02:01 +0800

    ..

--- 17codes/static/dist/js/lib.js
+++ 17codes/static/dist/js/lib.js
@@ -63,7 +63,7 @@
 /******/ 	__webpack_require__.p = "";
 /******/
 /******/ 	// Load entry module and return exports
-/******/ 	return __webpack_require__(__webpack_require__.s = 5);
+/******/ 	return __webpack_require__(__webpack_require__.s = 6);
 /******/ })
 /************************************************************************/
 /******/ ([
@@ -9530,7 +9530,8 @@ module.exports = g;
 
 /***/ }),
 /* 4 */,
-/* 5 */
+/* 5 */,
+/* 6 */
 /***/ (function(module, exports, __webpack_require__) {
 
 __webpack_require__(2);
--- 17codes/static/src/js/index.js
+++ 17codes/static/src/js/index.js
@@ -2,13 +2,13 @@
 //document.body.innerHTML = "111122233344455667893333331191"
 console.log('11');
 
-//import Vue from 'vue';
-//////import Vuex from 'vuex';
-//import router from 'vue-router';
-//import App from '../../../app.vue';
-//new Vue({
-//    el: '#app',
-//    router,
-//    //store,
-//    render: h => h(App)
-//});
+import Vue from 'vue';
+////import Vuex from 'vuex';
+import router from 'vue-router';
+import App from '../../../app.vue';
+new Vue({
+   el: '#app',
+   router,
+   //store,
+   render: h => h(App)
+});
