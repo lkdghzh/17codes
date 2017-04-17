d3be7360ffcf8a8d37315f62147bb133aa08d53d
like <416960428@qq.com>
2017-04-17 14:08:05 +0800

    废弃MVC,采用传统页面显示，前后端完全分离

--- 17codes/static/dist/js/lib.js
+++ 17codes/static/dist/js/lib.js
@@ -63,7 +63,7 @@
 /******/ 	__webpack_require__.p = "";
 /******/
 /******/ 	// Load entry module and return exports
-/******/ 	return __webpack_require__(__webpack_require__.s = 6);
+/******/ 	return __webpack_require__(__webpack_require__.s = 9);
 /******/ })
 /************************************************************************/
 /******/ ([
@@ -9531,7 +9531,10 @@ module.exports = g;
 /***/ }),
 /* 4 */,
 /* 5 */,
-/* 6 */
+/* 6 */,
+/* 7 */,
+/* 8 */,
+/* 9 */
 /***/ (function(module, exports, __webpack_require__) {
 
 __webpack_require__(2);
