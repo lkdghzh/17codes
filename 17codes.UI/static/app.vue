778012d8551f70568880e2590c956ffd1a250c4a
like <416960428@qq.com>
2017-04-28 14:15:21 +0800

    首頁佈局

--- 17codes.UI/static/app.vue
+++ 17codes.UI/static/app.vue
@@ -1,43 +1,70 @@
-﻿<style>
-
+﻿<style scope>
+.root-page{
+	height: 100%;
+}
+.nav-bar {
+	min-height: 50px;
+	height: 5.5%;
+	background-color:#C9CABB;
+}
+/*color0: #F5F6EB__RGB(245,246,235)
+color1: #EFF0DC__RGB(239,240,220)
+color2: #C9CABB__RGB(201,202,187)
+color3: #34352C__RGB(52,53,44)*/
+.nav-container {
+	margin: 0px auto;
+	height: 100%;
+	width: 980px;
+	border-radius: 5px;
+	background:#F5F6EB;
+}
+.aside-menu{
+	height:94.5%;
+	width: 20%;
+	background:#34352C;
+	float: left;
+}
+.article-body{
+	height:94.5%;
+	width: 80%;
+	background:#EFF0DC;
+	float: right;
+}
 </style>
 
 <template>
-	<nav class="navbar navbar-inverse navbar-fixed-top">
-		<div class="container">
-			<div class="navbar-header">
-				<a class="navbar-brand" href="#">17Codes</a>
-				<ul class="nav navbar-nav">
-					
-				<a class="navbar-brand" href="#">JavaScript</a>
-				<a class="navbar-brand" href="#">C#</a>
-				<a class="navbar-brand" href="#">SQL</a>
-				<li>
-					<form class="navbar-form ">
-						<div class="input-group">
-							<input type="text" x-webkit-speech="" placeholder="文章搜索" class="form-control" name="q" id="kw">
-							<span class="input-group-btn">
-								<button class="btn btn-default" type="button">
-									Go!
-								</button>
-							</span>
-						</div>
-					</form>
-				</li>
-			</ul>
+	<div class="root-page" >
+		<nav class="nav-bar">
+			<div class="nav-container">
+				<div class="nav-header">
+					<a class="nav-brand" href="#">17Codes</a>
+					<!--<ul class="nav navbar-nav">
+						<li>
+							<form class="navbar-form ">
+								<div class="input-group">
+									<input type="text" placeholder="文章搜索" class="form-control" />
+									<span class="input-group-btn">
+										<button class="btn btn-default" type="button">
+											Go!
+										</button>
+									</span>
+								</div>
+							</form>
+						</li>
+					</ul>-->
+				</div>
 			</div>
-			
-		</div>
-	</nav>
+		</nav>
+		<aside class="aside-menu">
+			ul>li*5
+		</aside>
+		<article class="article-body"></article>
+	</div>
 </template>
 
 <script>
-//require('./src/css/bootstrap.min.css');
 import './src/css/bootstrap.min.css'
-// require('./1.css');
-//import 1 from './1.css'
-
-// require('./css/common.css');
+import './src/css/common.css'
 
 export default {
 	data: function () {
